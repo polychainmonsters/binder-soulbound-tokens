f118cac65308e9e84c291df2debfe7b38696b918
Leif <56021265+etherleif@users.noreply.github.com>
2024-02-24 15:02:41 +0100

    extended tests for randomness

--- contracts/Randomness.sol
+++ contracts/Randomness.sol
@@ -18,12 +18,16 @@ contract Randomness is Initializable {
     mapping(uint128 => mapping(uint256 => uint8))
         public idToRandomnessHashExists;
 
+    /** CONSTRUCTOR AND INITIALIZE */
+
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
     }
 
     function initialize() public initializer {}
+
+    /** REQUEST RANDOMNESS */
 
     function requestRandomness(uint24 duration) public returns (uint128) {
         require(duration <= 1 days, "duration too long");
@@ -37,6 +41,8 @@ contract Randomness is Initializable {
         emit RandomnessRequestEvent(idCounter, duration);
         return idCounter;
     }
+
+    /** COMMIT & REVEAL RANDOMNESS */
 
     function commitRandomness(uint128 id, uint256 randomnessHash) public {
         require(idToRequest[id].commitEndtime != 0, "request does not exist");
@@ -83,6 +89,25 @@ contract Randomness is Initializable {
             );
         }
     }
+
+    /** READ RANDOMNESS */
+
+    function readRandomness(uint128 id) public view returns (uint256) {
+        // only returns if the reveal period is over and there is a valid randomness (which isn't 0)
+        RandomnessRequest memory request = idToRequest[id];
+
+        require(request.commitEndtime != 0, "request does not exist");
+        require(
+            block.timestamp > request.commitEndtime + request.duration,
+            "reveal period not over"
+        );
+
+        require(idToRandomness[id] != 0, "randomness not set");
+
+        return idToRandomness[id];
+    }
+
+    /** UTILS */
 
     function hash(uint256 randomness) public pure returns (uint256) {
         return uint256(keccak256(abi.encodePacked(randomness)));
--- test/Randomness.ts
+++ test/Randomness.ts
@@ -75,5 +75,121 @@ describe.only("Randomness", async () => {
         expect(exists).to.equal(1n);
       }
     });
+
+    // should not be able to commit randomness after the duration
+    it("should not be able to commit randomness after the duration", async () => {
+      const { randomness } = await loadFixture(deployFixture);
+
+      const duration = 5 * 60;
+      const { requestId, requestTimestamp } = await requestRandomness(
+        randomness,
+        duration
+      );
+
+      await ethers.provider.send("evm_increaseTime", [duration + 1]);
+
+      // create a random uint256 value
+      const randomValue = ethers.toBigInt(ethers.randomBytes(32));
+
+      // hash it via the contract
+      const hash = await randomness.hash(randomValue);
+
+      // we commit the hash
+      await expect(
+        randomness.commitRandomness(requestId, hash)
+      ).to.be.revertedWith("commit period over");
+    });
+
+    // should be able to reveal randomness, but only after the commit period is over
+    it("should be able to reveal randomness, but only after the commit period is over", async () => {
+      const { randomness } = await loadFixture(deployFixture);
+
+      const { requestId } = await requestRandomness(randomness);
+
+      const randoms = [];
+
+      for (let i = 0; i < 10; i++) {
+        // create a random uint256 value
+        const randomValue = ethers.toBigInt(ethers.randomBytes(32));
+        randoms.push(randomValue);
+
+        // hash it via the contract
+        const hash = await randomness.hash(randomValue);
+
+        // we commit the hash
+        await randomness.commitRandomness(requestId, hash);
+
+        // we check if we successfully committed the hash
+        const exists = await randomness.idToRandomnessHashExists(
+          requestId,
+          hash
+        );
+
+        expect(exists).to.equal(1n);
+      }
+
+      // we increase the time
+      await ethers.provider.send("evm_increaseTime", [300]);
+
+      for (let i = 0; i < 10; i++) {
+        await randomness.revealRandomness(requestId, randoms[i]);
+      }
+    });
+
+    // should not be able to reveal before the reveal period
+    it("should not be able to reveal before the reveal period", async () => {
+      const { randomness } = await loadFixture(deployFixture);
+
+      const { requestId } = await requestRandomness(randomness);
+
+      const randomValue = ethers.toBigInt(ethers.randomBytes(32));
+
+      await expect(
+        randomness.revealRandomness(requestId, randomValue)
+      ).to.be.revertedWith("commit period not over");
+    });
+
+    // should not be able to reveal after the reveal period
+    it("should not be able to reveal after the reveal period", async () => {
+      const { randomness } = await loadFixture(deployFixture);
+
+      const { requestId } = await requestRandomness(randomness);
+
+      const randomValue = ethers.toBigInt(ethers.randomBytes(32));
+
+      const hash = await randomness.hash(randomValue);
+      await randomness.commitRandomness(requestId, hash);
+
+      // that ends the commit period
+      await ethers.provider.send("evm_increaseTime", [300]);
+
+      // and that ends the reveal period
+      await ethers.provider.send("evm_increaseTime", [300]);
+
+      await expect(
+        randomness.revealRandomness(requestId, randomValue)
+      ).to.be.revertedWith("reveal period is over");
+    });
+
+    // should not be able to reveal with a invalid random number
+    it("should not be able to reveal with a invalid random number", async () => {
+      const { randomness } = await loadFixture(deployFixture);
+
+      const { requestId } = await requestRandomness(randomness);
+
+      const randomValue = ethers.toBigInt(ethers.randomBytes(32));
+
+      const hash = await randomness.hash(randomValue);
+      await randomness.commitRandomness(requestId, hash);
+
+      // that ends the commit period
+      await ethers.provider.send("evm_increaseTime", [300]);
+
+      const fakeRandomValue = ethers.toBigInt(ethers.randomBytes(32));
+
+      await expect(
+        randomness.revealRandomness(requestId, fakeRandomValue)
+      ).to.be.revertedWith("randomness hash does not exist");
+    });
   });
 });
