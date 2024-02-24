d55c3455173c3b97a83cb414bad3b84cb18e67e6
Leif <56021265+etherleif@users.noreply.github.com>
2024-02-24 10:56:56 +0100

    randomness updates

--- contracts/Randomness.sol
+++ contracts/Randomness.sol
@@ -8,12 +8,15 @@ contract Randomness is Initializable {
 
     struct RandomnessRequest {
         uint256 commitEndtime;
+        uint24 duration;
     }
 
     uint128 idCounter;
 
     mapping(uint128 => RandomnessRequest) public idToRequest;
     mapping(uint128 => uint256) public idToRandomness;
+    mapping(uint128 => mapping(uint256 => uint8))
+        public idToRandomnessHashExists;
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
@@ -27,20 +30,68 @@ contract Randomness is Initializable {
 
         uint256 commitEndtime = block.timestamp + duration;
         idToRequest[++idCounter] = RandomnessRequest({
-            commitEndtime: commitEndtime
+            commitEndtime: commitEndtime,
+            duration: duration
         });
 
         emit RandomnessRequestEvent(idCounter, duration);
         return idCounter;
     }
 
-    function commitRandomness(uint128 id, uint256 randomness) public {
+    function commitRandomness(uint128 id, uint256 randomnessHash) public {
         require(idToRequest[id].commitEndtime != 0, "request does not exist");
         require(
             block.timestamp <= idToRequest[id].commitEndtime,
             "commit period over"
         );
 
-        idToRandomness[id] = randomness;
+        require(
+            idToRandomnessHashExists[id][randomnessHash] == 0,
+            "already committed"
+        );
+
+        idToRandomnessHashExists[id][randomnessHash] = 1;
+    }
+
+    function revealRandomness(uint128 id, uint256 randomness) public {
+        RandomnessRequest memory request = idToRequest[id];
+
+        require(request.commitEndtime != 0, "request does not exist");
+        require(
+            block.timestamp > request.commitEndtime,
+            "commit period not over"
+        );
+
+        require(
+            block.timestamp <= request.commitEndtime + request.duration,
+            "reveal period is over"
+        );
+
+        // the randomness should actually fit the hash
+        require(
+            idToRandomnessHashExists[id][hash(randomness)] == 1,
+            "randomness hash does not exist"
+        );
+
+        // if there is no randomness yet, set it. Otherwise combine it with the newly committed randomness
+        if (idToRandomness[id] == 0) {
+            idToRandomness[id] = randomness;
+        } else {
+            idToRandomness[id] = combineRandomness(
+                idToRandomness[id],
+                randomness
+            );
+        }
+    }
+
+    function hash(uint256 randomness) public pure returns (uint256) {
+        return uint256(keccak256(abi.encodePacked(randomness)));
+    }
+
+    function combineRandomness(
+        uint256 randomness1,
+        uint256 randomness2
+    ) public pure returns (uint256) {
+        return uint256(keccak256(abi.encodePacked(randomness1, randomness2)));
     }
 }
--- test/Randomness.ts
+++ test/Randomness.ts
@@ -1,5 +1,6 @@
 import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
 import { expect } from "chai";
+import { Contract } from "ethers";
 import { ethers, upgrades } from "hardhat";
 
 describe.only("Randomness", async () => {
@@ -18,19 +19,61 @@ describe.only("Randomness", async () => {
   });
 
   describe("request randomness", async () => {
-    it("should be able to request randomness", async () => {
-      const duration = 5 * 60;
-
-      const { randomness } = await loadFixture(deployFixture);
+    const requestRandomness = async (
+      randomness: Contract,
+      duration: number = 300
+    ) => {
       const tx = await randomness.requestRandomness(duration);
+      await tx.wait();
       const receipt = await tx.wait();
 
       const requestTimestamp = (await receipt.getBlock()).timestamp;
       const requestId = receipt.logs[0].args[0];
 
+      return { requestId, requestTimestamp };
+    };
+
+    it("should be able to request randomness", async () => {
+      const duration = 5 * 60;
+
+      const { randomness } = await loadFixture(deployFixture);
+
+      const { requestId, requestTimestamp } = await requestRandomness(
+        randomness,
+        duration
+      );
+
       const request = await randomness.idToRequest(requestId);
 
-      expect(request).to.equal(requestTimestamp + duration);
+      expect(request[0]).to.equal(requestTimestamp + duration);
+      expect(request[1]).to.equal(duration);
+    });
+
+    it("should be able to commit randomness", async () => {
+      const { randomness } = await loadFixture(deployFixture);
+
+      const { requestId, requestTimestamp } = await requestRandomness(
+        randomness
+      );
+
+      for (let i = 0; i < 10; i++) {
+        // create a random uint256 value
+        const randomValue = ethers.toBigInt(ethers.randomBytes(32));
+
+        // hash it via the contract
+        const hash = await randomness.hash(randomValue);
+
+        // we commit the hash
+        await randomness.commitRandomness(requestId, hash);
+
+        // we check if we successfully committed the hash
+        const exists = await randomness.idToRandomnessHashExists(
+          requestId,
+          hash
+        );
+
+        expect(exists).to.equal(1n);
+      }
     });
   });
 });
